4873a866b754bfdf8c9d02de97248e833b6d9862
dematerializedcat <iwaysimon@gmail.com>
2026-02-20 18:37:16 +0800

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var speed := 450.0
+@export var acceleration := 2000.0
+@export var friction := 1800.0
+@export var jump_velocity := -550.0
+@export var gravity := 1500.0
+
+func _physics_process(delta):
+
+	# Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Jump
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Horizontal movement with acceleration
+	var direction = Input.get_axis("left", "right")
+
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+	move_and_slide()
